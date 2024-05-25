36fcb83badde9f686ed3d3848190390f81277b6c
hy <hy@gmail.com>
2024-05-25 11:41:58 +0800

    update message
    Signed-off-by: hy <hy@gmail.com>

--- proto_message_maker.cpp
+++ proto_message_maker.cpp
@@ -19,4 +19,27 @@ namespace tc
         return msg.SerializeAsString();
     }
 
+    std::string ProtoMessageMaker::MakeGamepadButtonMessage(int32_t buttons) {
+        tc::Message msg;
+        msg.set_type(tc::MessageType::kGamepadState);
+        auto gs = msg.mutable_gamepad_state();
+        gs->set_buttons(buttons);
+        return msg.SerializeAsString();
+    }
+
+    std::string ProtoMessageMaker::MakeGamepadTriggerMessage(bool left, int trigger_value) {
+        tc::Message msg;
+        msg.set_type(tc::MessageType::kGamepadState);
+        auto gs = msg.mutable_gamepad_state();
+        return msg.SerializeAsString();
+    }
+
+    std::string ProtoMessageMaker::MakeGamepadThumbMessage(bool left, int thumb_x, int thumb_y) {
+        tc::Message msg;
+        msg.set_type(tc::MessageType::kGamepadState);
+        auto gs = msg.mutable_gamepad_state();
+        return msg.SerializeAsString();
+    }
+
+
 }
--- proto_message_maker.h
+++ proto_message_maker.h
@@ -10,8 +10,13 @@ namespace tc
     class ProtoMessageMaker {
     public:
 
+        // deprecated
         static std::string MakeGamepadState(int32_t buttons, int32_t left_trigger, int32_t right_trigger, int32_t thumb_lx,
                                             int32_t thumb_ly, int32_t thumb_rx, int32_t thumb_ry);
+
+        static std::string MakeGamepadButtonMessage(int32_t buttons);
+        static std::string MakeGamepadTriggerMessage(bool left, int trigger_value);
+        static std::string MakeGamepadThumbMessage(bool left, int thumb_x, int thumb_y);
 
     };
 
