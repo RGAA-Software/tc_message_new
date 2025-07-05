39ea911657ea9f4e1fac5117f8a8e795b4e35694
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-07-05 15:29:10 +0800

    add proto coverter
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

---
+++ proto_converter.h
@@ -0,0 +1,21 @@
+//
+// Created by RGAA on 5/07/2025.
+//
+
+#ifndef GAMMARAY_PROTO_CONVERTER_H
+#define GAMMARAY_PROTO_CONVERTER_H
+
+#include <memory>
+
+namespace tc
+{
+
+    class Data;
+    class Message;
+
+    std::shared_ptr<tc::Data> ProtoAsData(std::shared_ptr<tc::Message> msg);
+    std::shared_ptr<tc::Data> ProtoAsData(tc::Message* msg);
+
+}
+
+#endif //GAMMARAY_PROTO_CONVERTER_H
