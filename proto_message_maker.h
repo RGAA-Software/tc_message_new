d0de2ca3bd6ef83e66942d9e013e003fb611bfca
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-07-06 22:48:59 +0800

    fix building android
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- proto_message_maker.h
+++ proto_message_maker.h
@@ -8,20 +8,21 @@
 namespace tc
 {
 
+    class Data;
     class Message;
 
     class ProtoMessageMaker {
     public:
 
-        static std::string MakeGamepadState(int32_t buttons, int32_t left_trigger, int32_t right_trigger, int32_t thumb_lx, int32_t thumb_ly,
+        static std::shared_ptr<Data> MakeGamepadState(int32_t buttons, int32_t left_trigger, int32_t right_trigger, int32_t thumb_lx, int32_t thumb_ly,
                                             int32_t thumb_rx, int32_t thumb_ry, const std::string& device_id, const std::string& stream_id);
 
         // only used for Android Touch
-        static std::string MakeMouseEventFromTouch(int32_t event, const std::string& mon_name, float x_ratio, float y_ratio, const std::string& device_id,
+        static std::shared_ptr<Data> MakeMouseEventFromTouch(int32_t event, const std::string& mon_name, float x_ratio, float y_ratio, const std::string& device_id,
                                                    const std::string& stream_id);
 
         // change monitor
-        static std::string MakeChangeMonitor(int index, const std::string& name, const std::string& device_id, const std::string& stream_id);
+        static std::shared_ptr<Data> MakeChangeMonitor(int index, const std::string& name, const std::string& device_id, const std::string& stream_id);
 
         // file trans begin
         static std::string s_file_conn_token_;
