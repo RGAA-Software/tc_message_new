c44fd8cd4b40c530612e8372807fc1b67e801597
Hector <hollow.ichigo.yoo@gmail.com>
2025-07-26 15:32:09 +0800

    add converter
    Signed-off-by: Hector <hollow.ichigo.yoo@gmail.com>

--- rp_proto_converter.cpp
+++ rp_proto_converter.cpp
@@ -23,4 +23,20 @@ namespace tc
         return nullptr;
     }
 
+    std::shared_ptr<tc::Data> MakeRpRawRenderMessage(const std::string& stream_id,
+                                                     const std::string& device_id,
+                                                     const std::string& msg,
+                                                     bool data_channel,
+                                                     bool run_through) {
+        tcrp::RpMessage rp_msg;
+        rp_msg.set_type(tcrp::kRpRawRenderMessage);
+        auto sub = rp_msg.mutable_raw_render_msg();
+        sub->set_msg(msg);
+        sub->set_data_channel(data_channel);
+        sub->set_stream_id(stream_id);
+        sub->set_device_id(device_id);
+        sub->set_run_through(run_through);
+        return RpProtoAsData(&rp_msg);
+    }
+
 }
--- rp_proto_converter.h
+++ rp_proto_converter.h
@@ -6,6 +6,7 @@
 #define GAMMARAY_RP_PROTO_CONVERTER_H
 
 #include <memory>
+#include <string>
 
 namespace tcrp
 {
@@ -18,5 +19,11 @@ namespace tc
 
     std::shared_ptr<tc::Data> RpProtoAsData(std::shared_ptr<tcrp::RpMessage> msg);
     std::shared_ptr<tc::Data> RpProtoAsData(tcrp::RpMessage *msg);
+
+    std::shared_ptr<tc::Data> MakeRpRawRenderMessage(const std::string& stream_id,
+                                                     const std::string& device_id,
+                                                     const std::string& msg,
+                                                     bool data_channel,
+                                                     bool run_through = false);
 }
 #endif //GAMMARAY_RP_PROTO_CONVERTER_H
